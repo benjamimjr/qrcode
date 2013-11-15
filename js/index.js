7e686f9b6b431ea58803203003ee69e2ff4f9e07
Benjamim <benjamim@benjamim.net>
2013-11-15 18:55:32 -0200

    label

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -56,14 +38,17 @@ var app = {
         
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-        scanner.scan( function (result) { 
-
+        scanner.scan(function (result) {
+            $("#retorno").text("Conteúdo\n" +
+					"Resultado: " + result.text + "\n" +
+					"Formato: " + result.format + "\n");
+ /*
             alert(	"Conteúdo\n" + 
 					"Resultado: " + result.text + "\n" + 
 					"Formato: " + result.format + "\n"
 					);  
 
-           /*
+          
 		   console.log("Conteúdo: \n" +
                 "texto: " + result.text + "\n" +
                 "formato: " + result.format + "\n");
@@ -91,5 +76,7 @@ var app = {
         );
 
     }
+
+
 
 };
